d31dedd8632e115323f9b538118dd97e9986f80f
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-05-24 19:02:41 +0100

    HTML & CSS latest Updates

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const changeDue = document.getElementById("change-due")
+const cash = document.getElementById("cash")
+const purchaseBtn = document.getElementById("purchase-btn")
+const drawerChangeList = document.querySelectorAll(".changes");
+
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
@@ -10,3 +15,18 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100],
 ];
+
+cid.forEach((change, index) => {
+  drawerChangeList[index].textContent += change[1];
+})
+
+const calculateChange = (inputValue) => {
+  if (parseFloat(inputValue) >= price) {
+    return parseFloat(inputValue) - price;
+  } else {
+    alert("Customer does not have enough money to purchase the item");
+    inputValue = "";
+  }
+}
+
+purchaseBtn.addEventListener("click", ()=> calculateChange(cash.value))
